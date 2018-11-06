eba651a3770312ffd8867c9633602e20905116e5
Rémi Calixte <remi.calixte@datadoghq.com>
2018-11-06 15:40:35 -0500

    Remove compiler flags

--- high_level.go
+++ high_level.go
@@ -10,8 +10,6 @@ import (
 	"unsafe"
 )
 
-type PyCompilerFlags C.PyCompilerFlags
-
 func Py_Main(args []string) error {
 	argc := C.int(len(args))
 	argv := make([]*C.wchar_t, argc, argc)
@@ -40,10 +38,6 @@ func Py_Main(args []string) error {
 }
 
 func PyRun_AnyFile(filename string) error {
-	return PyRun_AnyFileFlags(filename, nil)
-}
-
-func PyRun_AnyFileFlags(filename string, flags *PyCompilerFlags) error {
 	cfilename := C.CString(filename)
 	defer C.free(unsafe.Pointer(cfilename))
 
@@ -54,8 +48,9 @@ func PyRun_AnyFileFlags(filename string, flags *PyCompilerFlags) error {
 	if cfile == nil {
 		return fmt.Errorf("File %s cannot be opened", filename)
 	}
+	defer C.fclose(cfile)
 
-	ret := C.PyRun_AnyFileFlags(cfile, cfilename, (*C.PyCompilerFlags)(flags))
+	ret := C.PyRun_AnyFileFlags(cfile, cfilename, nil)
 
 	if ret == -1 {
 		return fmt.Errorf("An exception was raised during execution")
@@ -64,14 +59,10 @@ func PyRun_AnyFileFlags(filename string, flags *PyCompilerFlags) error {
 }
 
 func PyRun_SimpleString(command string) error {
-	return PyRun_SimpleStringFlags(command, nil)
-}
-
-func PyRun_SimpleStringFlags(command string, flags *PyCompilerFlags) error {
 	ccommand := C.CString(command)
 	defer C.free(unsafe.Pointer(ccommand))
 
-	ret := C.PyRun_SimpleStringFlags(ccommand, (*C.PyCompilerFlags)(flags))
+	ret := C.PyRun_SimpleStringFlags(ccommand, nil)
 
 	if ret == -1 {
 		return fmt.Errorf("An exception was raised during execution")
