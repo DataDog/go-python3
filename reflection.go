6642dca2d5f93b522c73962500123a305282110e
Rémi Calixte <remi.calixte@datadoghq.com>
2018-12-04 15:26:48 -0500

    Add reflection bindings

---
+++ reflection.go
@@ -0,0 +1,36 @@
+package python3
+
+/*
+#include "Python.h"
+*/
+import "C"
+
+//PyEval_GetBuiltins : https://docs.python.org/3/c-api/reflection.html?highlight=reflection#c.PyEval_GetBuiltins
+func PyEval_GetBuiltins() *PyObject {
+	return togo(C.PyEval_GetBuiltins())
+}
+
+//PyEval_GetLocals : https://docs.python.org/3/c-api/reflection.html?highlight=reflection#c.PyEval_GetLocals
+func PyEval_GetLocals() *PyObject {
+	return togo(C.PyEval_GetLocals())
+}
+
+//PyEval_GetGlobals : https://docs.python.org/3/c-api/reflection.html?highlight=reflection#c.PyEval_GetGlobals
+func PyEval_GetGlobals() *PyObject {
+	return togo(C.PyEval_GetGlobals())
+}
+
+//PyEval_GetFuncName : https://docs.python.org/3/c-api/reflection.html?highlight=reflection#c.PyEval_GetFuncName
+func PyEval_GetFuncName(pyFunc *PyObject) string {
+	cname := C.PyEval_GetFuncName(toc(pyFunc))
+
+	return C.GoString(cname)
+}
+
+//PyEval_GetFuncDesc : https://docs.python.org/3/c-api/reflection.html?highlight=reflection#c.PyEval_GetFuncDesc
+func PyEval_GetFuncDesc(pyFunc *PyObject) string {
+
+	cdesc := C.PyEval_GetFuncDesc(toc(pyFunc))
+
+	return C.GoString(cdesc)
+}
