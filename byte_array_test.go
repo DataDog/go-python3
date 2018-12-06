6948421e7e17fbd9c2c6b0d3fa7efbb6a3c33588
Rémi Calixte <remi.calixte@datadoghq.com>
2018-12-06 12:26:11 -0500

    Byte Types

---
+++ byte_array_test.go
@@ -0,0 +1,66 @@
+package python3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestByteArrayCheck(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	array1 := PyByteArray_FromStringAndSize(s1)
+	assert.True(t, PyByteArray_Check(array1))
+	assert.True(t, PyByteArray_CheckExact(array1))
+	defer array1.DecRef()
+}
+
+func TestByteArrayFromAsString(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	array1 := PyByteArray_FromStringAndSize(s1)
+	defer array1.DecRef()
+
+	assert.Equal(t, s1, PyByteArray_AsString(array1))
+}
+
+func TestByteArrayConcat(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+	s2 := "bbbbbbbb"
+
+	array1 := PyByteArray_FromStringAndSize(s1)
+	defer array1.DecRef()
+
+	bytes := PyBytes_FromString(s2)
+	assert.NotNil(t, bytes)
+	defer bytes.DecRef()
+
+	array2 := PyByteArray_FromObject(bytes)
+	assert.NotNil(t, array2)
+	defer array2.DecRef()
+
+	newArray := PyByteArray_Concat(array1, array2)
+	defer newArray.DecRef()
+
+	assert.Equal(t, s1+s2, PyByteArray_AsString(newArray))
+}
+
+func TestByteArrayResize(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	array1 := PyByteArray_FromStringAndSize(s1)
+	defer array1.DecRef()
+
+	length := 20
+	PyByteArray_Resize(array1, 20)
+
+	assert.Equal(t, length, PyByteArray_Size(array1))
+}
---
+++ bytes_test.go
@@ -0,0 +1,77 @@
+package python3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBytesCheck(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	bytes1 := PyBytes_FromString(s1)
+	assert.True(t, PyBytes_Check(bytes1))
+	assert.True(t, PyBytes_CheckExact(bytes1))
+	defer bytes1.DecRef()
+}
+
+func TestBytesFromAsString(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	bytes1 := PyBytes_FromString(s1)
+	defer bytes1.DecRef()
+
+	assert.Equal(t, s1, PyBytes_AsString(bytes1))
+}
+
+func TestBytesSize(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+
+	bytes1 := PyBytes_FromString(s1)
+	defer bytes1.DecRef()
+
+	assert.Equal(t, 8, PyBytes_Size(bytes1))
+}
+
+func TestBytesConcat(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+	s2 := "bbbbbbbb"
+
+	bytes1 := PyBytes_FromString(s1)
+
+	bytes2 := PyBytes_FromString(s2)
+	assert.NotNil(t, bytes2)
+	defer bytes2.DecRef()
+
+	bytes1 = PyBytes_Concat(bytes1, bytes2)
+	assert.NotNil(t, bytes1)
+	defer bytes1.DecRef()
+
+	assert.Equal(t, s1+s2, PyBytes_AsString(bytes1))
+}
+
+func TestBytesConcatAndDel(t *testing.T) {
+	Py_Initialize()
+
+	s1 := "aaaaaaaa"
+	s2 := "bbbbbbbb"
+
+	bytes1 := PyBytes_FromString(s1)
+
+	bytes2 := PyBytes_FromString(s2)
+	assert.NotNil(t, bytes2)
+
+	bytes1 = PyBytes_ConcatAndDel(bytes1, bytes2)
+	assert.NotNil(t, bytes1)
+	defer bytes1.DecRef()
+
+	assert.Equal(t, s1+s2, PyBytes_AsString(bytes1))
+}
