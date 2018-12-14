b6d5d0242d17115aee16a2a4ca6f1edce38e2a23
Rémi Calixte <remi.calixte@datadoghq.com>
2018-12-14 13:45:17 -0500

    Add some examples

---
+++ examples/python3/main.go
@@ -0,0 +1,31 @@
+/*
+Unless explicitly stated otherwise all files in this repository are licensed
+under the $license_for_repo License.
+This product includes software developed at Datadog (https://www.datadoghq.com/).
+Copyright 2018 Datadog, Inc.
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DataDog/go-python3"
+)
+
+func main() {
+	i, err := python3.Py_Main(os.Args)
+	if err != nil {
+		fmt.Printf("error launching the python interpreter: %s\n", err)
+		os.Exit(1)
+	}
+	if i == 1 {
+		fmt.Println("The interpreter exited due to an exception")
+		os.Exit(1)
+	}
+	if i == 2 {
+		fmt.Println("The parameter list does not represent a valid Python command line")
+		os.Exit(1)
+	}
+}
